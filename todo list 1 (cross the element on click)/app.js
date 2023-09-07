2159f75907d278e4e66256139cbfe802216d6e13
shantanuzeuslearning <142993060+shantanuzeuslearning@users.noreply.github.com>
2023-09-07 15:44:09 +0530

    app.js

--- todo list 1 (cross the element on click)/app.js
+++ todo list 1 (cross the element on click)/app.js
@@ -1,15 +1,36 @@
 const ul = document.querySelector("ul");
 const liItems = document.querySelectorAll("li");
 const btnAdd = document.querySelector("button");
+const newInput = document.getElementById("newTodo");
+
 btnAdd.addEventListener("click", () => {
-  const li = document.createElement("li");
-  li.textContent = "something new to do";
-  ul.prepend(li);
+  const taskText = newInput.value.trim();
+  if (taskText !== "") {
+    const li = document.createElement("li");
+    li.textContent = taskText;
+    li.addEventListener("click", toggleTaskCompletion);
+    ul.appendChild(li);
+    newInput.value = "";
+    btnAdd.disabled = true;
+  }
 });
 
 ul.addEventListener("click", (e) => {
-  console.log(e.target);
   if (e.target.tagName === "LI") {
-    e.target.style.textDecoration = "line-through";
+    toggleTaskCompletion(e.target);
   }
 });
+
+function toggleTaskCompletion(taskItem) {
+  taskItem.style.textDecoration = taskItem.style.textDecoration
+    ? ""
+    : "line-through";
+}
+
+function handleInputChange() {
+  const taskText = newInput.value.trim();
+  btnAdd.disabled = taskText === "";
+}
+
+newInput.addEventListener("input", handleInputChange);
+btnAdd.disabled = true;
