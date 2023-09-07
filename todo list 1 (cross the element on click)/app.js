9a6cc410f156f014a536a267fa67f31c3851a404
shantanuzeuslearning <142993060+shantanuzeuslearning@users.noreply.github.com>
2023-09-07 12:52:45 +0530

    Add files via upload

---
+++ todo list 1 (cross the element on click)/app.js
@@ -0,0 +1,15 @@
+const ul = document.querySelector("ul");
+const liItems = document.querySelectorAll("li");
+const btnAdd = document.querySelector("button");
+btnAdd.addEventListener("click", () => {
+  const li = document.createElement("li");
+  li.textContent = "something new to do";
+  ul.prepend(li);
+});
+
+ul.addEventListener("click", (e) => {
+  console.log(e.target);
+  if (e.target.tagName === "LI") {
+    e.target.style.textDecoration = "line-through";
+  }
+});
